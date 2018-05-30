126dd4be45e493530aab39303a6501b6ccb31a97
Francisco Fernandez <18franciscofernandez@gmail.com>
2018-05-30 18:21:25 -0300

    cambios

--- Project/perfil_usuario.php
+++ Project/perfil_usuario.php
@@ -19,6 +19,24 @@
  		</div>
 		<a href="./index.php"><img src="IMG/libros.jpg"></a>
 	</div>
+	ads
+	das
+	das
+	das
+	das
+	sa
+	d
+	sd
+	qwd
+	wq
+	dqw
+	dqw
+	dqw
+	ddqwqqdwdwddw
+	dqwdwqdqwdwqdwq
+	FOSFOOFASOSFAOOFAS
+	FASOFASOFAOSFOASFOAOSFA
+	FOASOFAOSOAOFAS
 	<div id="margenGeneral">
 		<h1 id="fontTitulo">Usuario</h1>
 		<div class="presentacionLibro">
